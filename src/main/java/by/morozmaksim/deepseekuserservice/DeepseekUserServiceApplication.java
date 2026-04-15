820b4d015826a006cff4f0d50d66c3f8d53a6374
Moroz Maksim <morozmaksim99@mail.ru>
2026-04-15 14:15:16 +0300

    init

---
+++ src/main/java/by/morozmaksim/deepseekuserservice/DeepseekUserServiceApplication.java
@@ -0,0 +1,13 @@
+package by.morozmaksim.deepseekuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepseekUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepseekUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/morozmaksim/deepseekuserservice/DeepseekUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.morozmaksim.deepseekuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepseekUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
